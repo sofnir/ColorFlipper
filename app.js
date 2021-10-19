a0949fc63772778bd46e034bfea657bfdf546fe8
Maciej <maciej.gwizdz1994@gmail.com>
2021-10-19 11:20:36 +0200

    Create color flippers

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color")
+
+btn.addEventListener('click', function(){
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor =  randomColor
+    color.textContent =  randomColor;
+    console.log(randomColor);
+})
+
+function getRandomColor() {
+    const randomNumber = Math.floor(Math.random() * colors.length);
+    return colors[randomNumber];
+}
--- hex.js
+++ hex.js
@@ -1,1 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color")
+
+btn.addEventListener('click', function(){
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor =  randomColor
+    color.textContent =  randomColor;
+    console.log(randomColor);
+})
+
+function getRandomColor() {
+    let randomHexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+        let randomNumber = Math.floor(Math.random() * hex.length);        
+        randomHexColor += hex[randomNumber];
+    }
+
+    return randomHexColor;
+}
